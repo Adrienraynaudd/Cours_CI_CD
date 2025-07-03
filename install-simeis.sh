881d913e64eefbe8b18432fc0aec510e9a676a9e
adrienraynaudd <adrien48632@gmail.com>
2025-07-03 14:22:04 +0200

    fix bug intaller

--- install-simeis.sh
+++ install-simeis.sh
@@ -7,8 +7,8 @@ sudo useradd --system --no-create-home --shell /usr/sbin/nologin simeis || echo 
 sudo install -m 755 ./target/release/simeis-server /usr/bin/simeis-server
 sudo chown root:root /usr/bin/simeis-server
 
-sudo install -Dm644 ./docs/simeis-server.1 /usr/share/man/man1/simeis-server.1
-sudo gzip -f /usr/share/man/man1/simeis-server.1
+sudo install -Dm644 ./docs/simeis-server /usr/share/man/man1/simeis-server
+sudo gzip -f /usr/share/man/man1/simeis-server
 
 sudo install -Dm644 ./supervisor/simeis-server.conf /etc/supervisor/conf.d/simeis-server.conf
 
