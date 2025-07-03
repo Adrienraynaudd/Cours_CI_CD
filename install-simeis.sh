17fa3a8c5ecc56cfcf11c949cb290f1cf83ed188
adrienraynaudd <adrien48632@gmail.com>
2025-07-03 13:38:03 +0200

    test

---
+++ install-simeis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+sudo useradd --system --no-create-home --shell /usr/sbin/nologin simeis || echo "✅ Utilisateur déjà existant"
+
+sudo install -m 755 ./target/release/simeis-server /usr/bin/simeis-server
+sudo chown root:root /usr/bin/simeis-server
+
+sudo install -Dm644 ./docs/simeis-server.1 /usr/share/man/man1/simeis-server.1
+sudo gzip -f /usr/share/man/man1/simeis-server.1
+
+sudo install -Dm644 ./supervisor/simeis-server.conf /etc/supervisor/conf.d/simeis-server.conf
+
+sudo supervisorctl reread
+sudo supervisorctl update
+
+sudo supervisorctl start simeis-server
+
+sudo supervisorctl status simeis-server
